919066db018a6b9658945f193ef897dad092cd7d
jiayilee97 <jiayilee97@u.nus.edu>
2020-02-11 16:56:13 +0800

    z

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ app.get('/', (req, res) => {
 app.listen(3000, () => console.log('Server running on port 3000'))
 */
 
-
+/*
 const express = require('express')
 const app = express()
 
 app.use(express.static('public'))
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.listen(3000, () => console.log('Server running on port 3000'))*/
 
 
 
-/*
+
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
-var privateKey  = fs.readFileSync('key.pem', 'utf8');
-var certificate = fs.readFileSync('cert.pem', 'utf8');
+var privateKey  = fs.readFileSync('/etc/letsencrypt/live/www.leejiayi.net/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/www.leejiayi.net/fullchain.pem', 'utf8');
 
 var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
@@ -34,4 +34,4 @@ var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(8080, () => console.log('Server running on port 8080'));
-httpsServer.listen(8443);*/
+httpsServer.listen(8443);
