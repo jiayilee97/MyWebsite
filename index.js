262cf44fe7d702d2db6166b29c7106ddf7306a1f
jiayilee97 <jiayilee97@u.nus.edu>
2020-02-11 10:55:02 +0800

    z

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const express = require('express')
+/*const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
   res.send('HEY!')
 })
 
+app.listen(3000, () => console.log('Server running on port 3000'))
+*/
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
