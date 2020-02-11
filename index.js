89706efaf74618b9eab152f8996a6cc320d9a713
jiayilee97 <jiayilee97@u.nus.edu>
2020-02-11 16:47:40 +0800

    z

--- index.js
+++ index.js
@@ -6,8 +6,32 @@ app.get('/', (req, res) => {
 
 app.listen(3000, () => console.log('Server running on port 3000'))
 */
+
+/*
 const express = require('express')
 const app = express()
 
 app.use(express.static('public'))
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.listen(3000, () => console.log('Server running on port 3000'))*/
+
+
+
+
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('key.pem', 'utf8');
+var certificate = fs.readFileSync('cert.pem', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
+var app = express();
+
+// your express configuration here
+app.use(express.static('public'))
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080, () => console.log('Server running on port 8080'));
+httpsServer.listen(8443);
